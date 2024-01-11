1d8f05ddf2b76a9b9fb215bfa150faf1c8975a87
AI-Pro-Pravin <shettipravin@gmail.com>
2024-01-11 20:06:38 +0530

    Web application

---
+++ app.py
@@ -0,0 +1,27 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+
+app = Flask(__name__)
+## Load the model
+model=pickle.load(open('regmodel.pkl','rb'))
+scaled=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaled.transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True) 
